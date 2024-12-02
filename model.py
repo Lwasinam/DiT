7529ec41dadaac40179d40a765500c6fbc6976b9
Lwasinam <lwasinamdilli@gmail.com>
2024-12-02 17:38:18 +0100

    multi head attention

--- model.py
+++ model.py
@@ -52,22 +52,48 @@ def positional_encoding(patch_size, embed_dim):
     # print(x.shape)
 
     return torch.cat([x,y],dim=1)       
-class MultiHeadAttention():
+class MultiHeadAttention(nn.Module):
     def __init__(self, embed_dim, num_heads):
         super().__init__()
+        assert embed_dim % num_heads == 0, "Embedding dimension must be divisible by the number of heads."
+
         self.embed_dim = embed_dim
         self.num_heads = num_heads
         self.head_dim = embed_dim // num_heads
+
         self.wq = nn.Linear(embed_dim, embed_dim)
         self.wk = nn.Linear(embed_dim, embed_dim)
         self.wv  = nn.Linear(embed_dim, embed_dim)
         self.ww = nn.Linear(embed_dim, embed_dim)
 
-    def attention(x) :
+    def compute_attention(self, query, key,  value, ) :
+        
+        attention  = (query @ key.transpose(-2,-1))/math.sqrt(self.head_dim)
+        attention = attention.softmax(dim=-1)  # (batch_size, num_heads, query_len, key_len)
+        attention = attention @ value 
+        return attention
+
+     
+
+    def forward(self, x):
         wQ = self.wq(x)
         wK = self.wk(x)
         wV = self.wv(x)
-        wW = self.ww(x)
+        
+        
+        wQ = wQ.view(wQ.shape[0], wQ.shape[1], self.num_heads, self.head_dim).transpose(1,2)
+        wK = wK.view(wK.shape[0], wK.shape[1], self.num_heads, self.head_dim).transpose(1,2) 
+        wV = wV.view(wV.shape[0], wV.shape[1], self.num_heads, self.head_dim).transpose(1,2)
+
+        out = self.compute_attention(wQ, wK, wV)
+
+        out = out.transpose(1, 2).contiguous().view(out.shape[0], out.shape[1], self.embed_dim)
+
+
+        return self.ww(out) 
+
+
+
 
 
         
